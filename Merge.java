ac085a6deddbedfd0bc59a6df2aa957e785757f3
Julian Wong <jwong01@stuy.edu>
2019-03-18 22:49:36 -0400

    split array into two, nothing done with them yet

--- Merge.java
+++ Merge.java
@@ -1,13 +1,18 @@
+import java.util.Arrays;
 public class Merge {
   public static void mergesort(int[] dat) {
     msh(dat, 0, dat.length - 1);
   }
   public static void msh(int[] dat, int l, int h) {
     if(l >= h) {return;}
-    msh(dat, 0, (dat.length - 1) / 2);
-    msh(dat, (dat.length - 1) / 2 + 1, dat.length - 1);
+    int[] lra = Arrays.copyOfRange(dat, 0, (dat.length + 1) / 2);
+    int[] hra = Arrays.copyOfRange(dat, (dat.length + 1) / 2, dat.length);
   }
   public static void main(String[] javabeans) {
-    
+    int[] da = {8,6,7,5,2,4,0,3,1,9};
+    mergesort(da);
+    for(int q = 0; q < da.length; q++) {
+      System.out.print(da[q] + " ");
+    }
   }
 }
